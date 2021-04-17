d8173f45fec6eb4f761a3849e0c9eaf6623a08a6
Kevin_Gutierrez <kgutierrez@passer.cr>
2021-04-17 11:01:42 -0600

    clean code

--- src/resolvers/ping.resolver.ts
+++ src/resolvers/ping.resolver.ts
@@ -1,5 +1,4 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { addAgency,getAgency } from "factura-middle"
 
 @Resolver()
 export class PingResolver{
@@ -11,23 +10,6 @@ export class PingResolver{
     @Mutation(() => String)
     async hello(
         @Arg("name") name: string) {
-            var agency = {
-                phone:{
-                  e164: "+50671902321",
-                  countryCode:"506",
-                  nationalNumber:"71902321"
-                },
-                address:{
-                  country:"Costa Rica",
-                  address:"Miravalles",
-                  province:"San Jose"
-                  
-                },
-                agencyName:"Agencia #3"
-              }
-
-            var prueba = await getAgency("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDViZjg5M2MzNDkwZGEwNTc3YTM1YmMiLCJuYW1lIjoiS0VWSU4iLCJleHAiOjE2MjE4ODIyNjYuNTYyLCJpYXQiOjE2MTY2OTgyNjZ9.0KogCv0w63Yc2rKX84lBW13OU_Kcd-Xx3LrjwOt4_0E");
-            console.log(prueba);
 
         return `Hello my friend ${name}`;
     }
